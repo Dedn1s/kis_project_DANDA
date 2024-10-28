ac69bcf29f61fb6dcf80366bfc36af024ce21dbc
Arina <wolf.arishka@gmailcom>
2024-10-28 19:37:06 +0300

    добавила первый варинат баннера

--- app/page.js
+++ app/page.js
@@ -1,8 +1,12 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Banner from "./components/Banner";
 
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+    <div>
+      <h1>Hello World</h1>
+      <Banner/>
+    </div>
   );
 }
