badd943a50845f1d323487a742e6060e8d158cbf
Arina <wolf.arishka@gmailcom>
2024-10-28 23:13:18 +0300

    добавила секцию брендов

--- app/page.js
+++ app/page.js
@@ -1,12 +1,15 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Banner from "./components/Banner";
+import BrandSlider from "./components/Brends";
 
 export default function Home() {
   return (
     <div>
       <h1>Hello World</h1>
       <Banner/>
+      
+      <BrandSlider />
     </div>
   );
 }
