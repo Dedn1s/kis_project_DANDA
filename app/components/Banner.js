a3ca63bd1ccef6c34ef6300b56e2f08fbb9523e9
Arina <wolf.arishka@gmailcom>
2024-10-29 19:31:31 +0300

    отредактировала баннер, он стал красивее

--- app/components/Banner.js
+++ app/components/Banner.js
@@ -1,6 +1,6 @@
 'use client';
 
-import styles from "./../page.module.css";
+import styles from "./Banner.module.css";
 
 export default function Banner() {
     return (
@@ -12,18 +12,22 @@ export default function Banner() {
               косметика<br />
               и хозтовары
           </h1>
-            <h2>оптом по кокчетаву и области</h2>
-            <button className={styles.banner__content__button} onClick={() => alert('Страница в разработке')}>
+          <h2 className={styles.banner__subtitle}>оптом по кокчетаву и области</h2>
+            <button className={styles.banner__content__button} onClick={() => alert('У программиста лапки, она не успела сделать эту кнопку')}>
               В каталог
             </button>
             <div className={styles.banner__details}>
               <div>
-                <span>+</span>
-                <p>Только самые выгодные предложения</p>
+              <p>
+              <span className={styles.plusCircle}>+</span> Только самые <br />
+                  выгодные предложения
+                </p>
               </div>
               <div>
-                <span>+</span>
-                <p>Бесплатная доставка по Кокчетаву от 10 тыс ₸</p>
+              <p>
+                  <span className={styles.plusCircle}>+</span> Бесплатная доставка <br />
+                  по Кокчетаву от 10 тыс ₸
+                </p>
               </div>
             </div>
           </div>
