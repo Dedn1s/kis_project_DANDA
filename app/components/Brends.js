5ac1bcf23aa2801a8125720d7561cc81e549610a
Arina <wolf.arishka@gmailcom>
2024-10-31 23:11:59 +0300

    добавила адаптив для лучших товаров

--- app/components/Brends.js
+++ app/components/Brends.js
@@ -1,8 +1,9 @@
 "use client";
 
 import Image from "next/image";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import styles from "./BrandSlider.module.css";
+
 
 export default function BrandSlider() {
   const [currentSlide, setCurrentSlide] = useState(0);
@@ -10,18 +11,30 @@ export default function BrandSlider() {
     "/airwick.png", "/masterfresh.png", "/brand2.png",
     "/brand3.png", "/brand5.png", "/brand6.png",
     "/brand7.png", "/brand8.avif", "/listik.png",
-    "/image3.png", "/images/brand11.jpg", "/images/brand12.jpg",
-    "/images/brand13.jpg", "/images/brand14.jpg", "/images/brand15.jpg",
-    "/images/brand16.jpg", "/images/brand17.jpg", "/images/brand18.jpg",
-    "/images/brand19.jpg", "/images/brand20.jpg", "/images/brand21.jpg",
-    "/images/brand22.jpg", "/images/brand23.jpg", "/images/brand24.jpg",
-    "/images/brand25.jpg", "/images/brand26.jpg", "/images/brand27.jpg",
-    "/images/brand28.jpg", "/images/brand29.jpg", "/images/brand30.jpg",
+    "/image3.png", "/airwick.png", "/airwick.png",
+    "/airwick.png", "/airwick.png", "/airwick.png",
+    "/airwick.png", "/airwick.png", "/airwick.png",
+    "/airwick.png", "/airwick.png", "/airwick.png",
+    "/airwick.png", "/airwick.png", "/airwick.png",
+    "/airwick.png", "/airwick.png", "/airwick.png",
+    "/airwick.png", "/airwick.png", "/airwick.png",
   ];
 
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => setIsMobile(window.innerWidth <= 768);
+
+    handleResize(); 
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   const groupedImages = [];
-  for (let i = 0; i < brandImages.length; i += 10) {
-    groupedImages.push(brandImages.slice(i, i + 10));
+  const imagesToDisplay = isMobile ? brandImages.slice(0, 10) : brandImages;
+
+  for (let i = 0; i < imagesToDisplay.length; i += 10) {
+    groupedImages.push(imagesToDisplay.slice(i, i + 10));
   }
 
   const handleDotClick = (index) => {
