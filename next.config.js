ea60b744df64f30db431633b60b468bffa862d5e
Dedn1s <165265259+Dedn1s@users.noreply.github.com>
2024-11-13 20:52:39 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,8 +2,13 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
+    
     output: 'export',
     distDir: 'dist',
+    images:{
+        unoptimized: true,
+    },
+    
   }
    
   module.exports = nextConfig
