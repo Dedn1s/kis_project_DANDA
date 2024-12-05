4dc9de2c8abeac9c1598722e6556aba4d8dd323f
Glinsky-Nikita <nikitos12122004@gmail.com>
2024-12-05 09:16:11 +0300

    хлебные крошки недоделанные

---
+++ app/[lng]/components/Breadcrumb/Breadcrumb.js
@@ -0,0 +1,55 @@
+import styles from '@/components/Breadcrumb/Breadcrumb.module.css';
+import useWindowSize from '@/core/hooks/useWindowSize';
+import Link from 'next/link';
+import { useTranslation } from 'next-i18next';
+
+const Breadcrumb = ({ product }) => {
+  const { isTablet, isMobile } = useWindowSize();
+  const { t } = useTranslation('common');
+
+  const breadcrumbItems = [
+    { name: t('Main'), path: '/' },
+    { name: t('Catalog'), path: '/catalog' },
+    {
+      name: ${product.shortNameRu},
+      path: /product/${product.id},
+    },
+  ];
+
+  if (isMobile || isTablet) {
+    return (
+      <div className={styles.container}>
+        <div className={styles.backButton}>
+          <Link
+            href="/"
+            style={{ textDecoration: 'none' }}
+            className={styles.backLink}
+          >
+            <span className={styles.backSymbol}>&lt;</span>
+            <span className={styles.backText}>{t('Back')}</span>
+          </Link>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <nav aria-label="breadcrumb">
+      <ul className={styles.breadcrumb}>
+        {breadcrumbItems.map((item, index) => (
+          <li key={index} className={styles.breadcrumbItem}>
+            {index < breadcrumbItems.length - 1 ? (
+              <Link href={item.path} className={styles.link}>
+                {item.name}
+              </Link>
+            ) : (
+              <span className={styles.currentItem}>{item.name}</span>
+            )}
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Breadcrumb;
